acc8e182df4401c24be2656005a531cc03c80183
gaylemcclure <gaylekdennison@gmail.com>
2024-10-07 17:35:29 +1100

    updated vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-  server: {
-    // port: 3000,
-    // open: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false,
+// // export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     "process.env": process.env,
+//   },
+//   server: {
+//     // port: 3000,
+//     // open: true,
+//     proxy: {
+//       "/api": {
+//         target: `http://localhost:${VITE_SERVER_PORT}`,
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// // });
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      "process.env": process.env,
+    },
+    server: {
+      // port: 3000,
+      // open: true,
+      proxy: {
+        "/api": {
+          target: `http://localhost:${process.env.VITE_SERVER_PORT}`,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+  });
+};
