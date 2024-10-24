7bfd6f56dc23a1b22864e7a7f536ef51857821e6
gaylemcclure <gaylekdennison@gmail.com>
2024-10-25 00:43:15 +1100

    changed path

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const startApolloServer = async () => {
 
   const imgconfig = multer.diskStorage({
     destination: (req, file, callback) => {
-      callback(null, "uploads");
+      callback(null, "../client/public/uploads");
     },
     filename: (req, file, callback) => {
       callback(null, `image.${file.originalname}`);
@@ -59,13 +59,12 @@ const startApolloServer = async () => {
     res.send(file);
   });
 
-  app.use("/uploads", express.static(path.join(__dirname, "../client/public")));
-  app.use("/uploads", express.static("./uploads"));
+  app.use("/uploads", express.static(path.join(__dirname, "../client/public/uploads")));
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.use("/uploads", express.static(path.join(__dirname, "../client/public")));
+    app.use("/uploads", express.static(path.join(__dirname, "../client/public/uploads")));
 
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
