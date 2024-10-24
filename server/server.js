bcb5a938f6101bc0e8190a0164e4f85ef7c54f57
gaylemcclure <gaylekdennison@gmail.com>
2024-10-25 00:50:41 +1100

    testing which item

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const startApolloServer = async () => {
 
   const imgconfig = multer.diskStorage({
     destination: (req, file, callback) => {
-      callback(null, "../client/public/uploads");
+      callback(null, "../client/public/poop");
     },
     filename: (req, file, callback) => {
       callback(null, `image.${file.originalname}`);
