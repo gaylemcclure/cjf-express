1d59715515951782aa5198e61d02bf58a58da2e5
gaylemcclure <gaylekdennison@gmail.com>
2025-02-06 19:16:50 +1100

    added date to header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -28,10 +28,14 @@ const Header = () => {
   return (
     <Navbar className="navbar flex flex-row justify-start">
       {/* Logo section */}
-      <div className="logo-wrapper">
+      <div className="logo-wrapper flex flex-row">
         <a href="/">
           <LogoImg src={logo} alt="logo" />
         </a>
+        <div className="pl-4 m-auto">
+          <h1 className="text-white text-lg mb-0 leading-none">5th - 8th</h1>
+          <h1 className="text-white text-lg leading-none">June 2025</h1>
+        </div>
       </div>
       {/* Links section */}
       <div className="links-wrapper flex ml-auto space-x-2">
