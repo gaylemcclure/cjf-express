0587427383ad36c347984d72781dd5c2c32851d5
gaylemcclure <gaylekdennison@gmail.com>
2025-03-24 13:36:57 +1100

    updated image urls

--- client/src/pages/bandPage.jsx
+++ client/src/pages/bandPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const BandPage = () => {
   const [bands, setBands] = useState();
@@ -28,20 +29,54 @@ const BandPage = () => {
   const bandTemplate = (band, image, i) => {
     return (
       <div className="band-template" key={i}>
-        <h1>{band}</h1>
-        <img href={image} />
+        {/* <h1>{band}</h1> */}
+        <BandImg className="band-imgs" src={image} />
+        <div class="band-name-container">
+          <p className="text-white text-center mb-0">{band}</p>
+        </div>
       </div>
     );
   };
 
   return (
-    <>
-      {bands?.map((b, i) => {
-        console.log(b);
-        return bandTemplate(b.fields["Band Name"], b.fields.Image_Link, i);
-      })}
-    </>
+    <BandPageWrapper>
+      <BandWrapper>
+        {bands?.map((b, i) => {
+          console.log(b);
+          return bandTemplate(b.fields["Band Name"], b.fields.Image_Link, i);
+        })}
+      </BandWrapper>
+    </BandPageWrapper>
   );
 };
+
+const BandPageWrapper = styled.div`
+  margin: 6rem 4rem;
+
+  .band-name-container {
+    background-color: black;
+    max-width: 300px;
+    height: 56px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0 0 8px 8px;
+  }
+`;
+
+const BandWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 2rem;
+  justify-content: center;
+`;
+
+const BandImg = styled.img`
+  height: 300px;
+  width: 300px;
+  object-fit: cover;
+  border-radius: 8px 8px 0 0;
+`;
 
 export default BandPage;
